41cd838332005c9c42de1ed13b6ce146c5a8237f
vira.murray <vira.avramenko@gmail.com>
2021-10-24 19:27:39 +0300

    added globally shared ctx

--- js/modules.js
+++ js/modules.js
@@ -1,7 +1,6 @@
 var ui ={};
 ui.navigation=`
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-// <div class="container-fluid">
   <a class="navbar-brand" href="#" onclick="defaultModule()">BankAPP</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
